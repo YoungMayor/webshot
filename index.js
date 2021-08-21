e0c8bd1172cf161793bf078617c2ac12ab8354e2
YoungMayor <youngmayor08@gmail.com>
2021-08-21 14:11:30 +0100

    Renamed the package from screenshot-share to webshot, Added docblocks and updated the package.json

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import DomToImage from "dom-to-image";
 
+/**
+ * Screenshot the content of an HTML element 
+ * 
+ * @param {string} elemID The ID of the element to convert to image
+ * 
+ * @returns Promise()
+ */
 async function __getImage(elemID) {
     const node = document.getElementById(elemID);
 
@@ -14,7 +21,15 @@ async function __getImage(elemID) {
         });
 }
 
-export async function save(elemID, fileName = "screenshot.jpg") {
+/**
+ * Download the contents of an HTML element with ID (elemID) as an 
+ * 
+ * @param {string} elemID The ID of the element to convert to an image
+ * @param {string} fileName The name to save the downloaded file as
+ * 
+ * @returns Promise()
+ */
+export async function download(elemID, fileName = "screenshot.jpg") {
     return __getImage(elemID).then((imageUrl) => {
         let link = document.createElement("a");
         link.href = imageUrl;
@@ -23,6 +38,15 @@ export async function save(elemID, fileName = "screenshot.jpg") {
     });
 }
 
+/**
+ * Share the contents of an HTML element as an Image on supported devices
+ * 
+ * @param {string} elemID The ID of the element to convert to an image
+ * @param {string} fileName The name to save the downloaded file as
+ * @param {Object} payload The extra share data
+ * 
+ * @returns Promise()
+ */
 export async function share(elemID, fileName = "screenshot.jpg", payload = {}) {
     return __getImage(elemID).then(async(imageUrl) => {
         const filesArray = [];
@@ -58,7 +82,14 @@ export async function share(elemID, fileName = "screenshot.jpg", payload = {}) {
     });
 }
 
+/**
+ * Default Export
+ * 
+ * Methods 
+ *  download(elemID, fileName)
+ *  share(elemID, fileName, data)
+ */
 export default {
-    save,
+    download,
     share
 }
